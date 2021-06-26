9325dfe53c5db8171708e5d25326f88e819c5deb
ikalpesh <kalpeshmahajan90@gmail.com>
2021-06-26 20:34:54 +0530

    Uploading File

---
+++ src/com/Day4/SnakeLadder/Main.java
@@ -0,0 +1,8 @@
+package com.Day4.SnakeLadder;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/Day4/SnakeLadder/UC2.java
@@ -0,0 +1,9 @@
+package com.Day4.SnakeLadder;
+
+public class UC2 {
+    public static void main(String[] args) {
+        int playerPosition = 0;
+        int die = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+        System.out.println("Dice roll: " + die);
+    }
+}
